954350b796cbe57d5d28554ad86de1d57f8c10ae
Estela Sharp <estelasharp@Estelas-MacBook.local>
2023-12-03 02:31:59 -0500

    upload for completion

--- index.js
+++ index.js
@@ -1,41 +1,104 @@
 //Group 4
-const cohort = "2310-GHP-ET-WEB-FT-SF";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}/events`;
+//ELEMENTS
+//CONTAINERS
+const eventContainer = document.getElementById('eventList')
+const formContainer = document.querySelector('#addEvent')
+formContainer.addEventListener("submit", newEvent)
 
-const state = {
-    events: [],
+let state = {
+    events: []
 };
 
-const eventList = document.querySelector("#events");
+//API URL 
+const APIurl = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events'
 
-async function render() {
-    await getEvents();
-    renderEvents();
-};
-render();
-
-async function getEvents() {
+//FETCH EVENTS
+async function fetchAPIevents() {
     try {
-        const response = await fetch(API_URL);
-        const json = await response.json();
-        state.events = json.data;
-        return json;
+        const APIresponse = await fetch(APIurl);
+        const APIconverted = await APIresponse.json();
+        state.events = APIconverted.data
+        console.log(state.events)
+        return state.events;
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
-};
+}
 
-function renderEvents() {
-    if (!state.events.length) {
-        eventList.innerHTML = "<li>No Events.</li>";
+//RENDER ALL EVENTS FROM STATE
+function renderEvents() { 
+
+    if (!state.events.length || state.events.length === 0) {
+        eventContainer.innerHTML = '<h3>No events found</h3>';
         return;
     }
-    const eventInfo = state.events.map((evnt) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<h2>${evnt.name}</h2>
-     <p>Date & Time: ${evnt.date}</p><p>Location: ${evnt.location}</p>
-        <p>${evnt.description}</p>`;
-        return li;
-    });
-    eventList.replaceChildren(...eventInfo);
+
+    const eventInfo = state.events.map((ele, ind) => {
+        const event = document.createElement('div')
+        event.className = `event`
+        event.innerHTML = `
+            <h3>Event: ${ele.name}</h3>
+            <h4>When: ${ele.date}</h4>
+            <h4>Where: ${ele.location}</h4>
+            <h4>ID: ${ele.id}</h4>
+            <p>Description: <br/>${ele.description}</p>
+            `;
+
+//DELETE BUTTON
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = "Delete Event"
+        event.append(deleteButton)
+
+        deleteButton.addEventListener("click", () => deleteEvent(ele.id))
+
+        return event;
+    })
+    eventContainer.replaceChildren(...eventInfo)
+}
+
+//INITIALIZATION FOR PAGE LOAD
+async function initialize() {
+    await fetchAPIevents();
+    renderEvents();
+}
+
+initialize();
+
+//DELETE
+async function deleteEvent(id) {
+    try {
+        const response = await fetch(`${APIurl}/${id}`, { 
+            method: "DELETE",
+        })
+        initialize()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+//UPDATE NEW EVENTS
+async function newEvent(e) {
+    e.preventDefault();
+
+    console.log(formContainer.name.value)
+    try {
+        const response = await fetch(APIurl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                name: formContainer.name.value,
+                date: `${formContainer.date.value}:00.000Z`,
+                location: formContainer.location.value, 
+                description: formContainer.description.value
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to create new event")
+        };
+        initialize();
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
